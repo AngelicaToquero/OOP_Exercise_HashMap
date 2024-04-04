39f452241d9be26c7b7f8a5edb28b1a122e2a0a9
Angelica Toquero <152822754+AngelicaToquero@users.noreply.github.com>
2024-04-04 15:21:28 +0800

    //

--- BookDA.java
+++ BookDA.java
@@ -7,38 +7,66 @@ public class BookDA {
     private HashMap<String, Book> bookMap;
 
     public BookDA() {
+         this.bookMap = new HashMap<>();
+    }
+    
+    public void addBook(Book book) {
+        bookMap.put(book.getIsbn(), book);
+    }
 
+    public Book getBookByIsbn(String isbn) {
+        return bookMap.get(isbn);
+    }
+
+        HashMap<String, String[]> bookMap = new HashMap<>();
+
+        // Read Book.csv and populate HashMap
         try {
-            Scanner bookFile = new Scanner(new FileReader("Book.csv"));
-            bookFile.nextLine();
-
-            while (bookFile.hasNext()) {
-
-                bookMap = new HashMap<>();
-                String bookLineData = new String();
-                bookLineData = bookFile.nextLine();
-
-                String[] bookLineDataSpecific = new String[3];
-                bookLineDataSpecific = bookLineData.split(",");
-
-                Book book = new Book();
-                book.setIsbn(bookLineDataSpecific[0].trim());
-                book.setTitle(bookLineData);(bookLineDataSpecific[1].trim());
-                book.setAuthor((bookLineDataSpecific[2]));
+            FileReader fileReader = new FileReader("Book.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] bookData = line.split(",");
+    
+                if (bookData.length >= 3) {
+                    String isbn = bookData[0].trim();
+                    String title = bookData[1].trim();
+                    String author = bookData[2].trim();
+                    bookMap.put(isbn, new String[]{title, author});
+                } else {
+                    
+                    System.out.println("Incomplete data in line: " + line);
+                }
             }
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+
+        HashMap<String, String> authorMap = new HashMap<>();
+        try {
+            FileReader fileReader = new FileReader("Author.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] authorData = line.split(",");
+                String authorName = authorData[0].trim();
+                String bio = authorData[1].trim();
+                authorMap.put(authorName, bio);
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Display report
+        for (String isbn : bookMap.keySet()) {
+            String[] bookData = bookMap.get(isbn);
+            String title = bookData[0];
+            String authorName = bookData[1];
+            String bio = authorMap.get(authorName);
+            System.out.println(isbn + " " + title);
+            System.out.println("\t" + authorName + " - " + bio);
         }
     }
-    private void printReport(Book book) {
-        for (Map.Entry<String, Book> entryMap : book.getBookMap().entrySet()) {
-            System.out.print(entryMap.getValue().getIsbn());
-            System.out.print(entryMap.getValue().getTitle());
-            System.out.print(entryMap.getValue().getAuthor());
-            System.out.println(entryMap.getValue().getBio());
-        }
-        System.out.println();
-    }
-    }
-   
